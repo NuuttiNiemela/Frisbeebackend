c3dbbd52964ab0ec4e13feb80483aae699ef476a
NuuttiNiemela <nuutti.niemela@hotmail.com>
2019-07-30 09:43:29 +0300

    lisätään pohjaversio

---
+++ src/main/java/fi/academy/frisbeebackend/FrisbeebackendApplication.java
@@ -0,0 +1,13 @@
+package fi.academy.frisbeebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrisbeebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrisbeebackendApplication.class, args);
+    }
+
+}
