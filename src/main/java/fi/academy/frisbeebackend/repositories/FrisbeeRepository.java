0a534aa766ef98e2e430c9a1919ae42cb5a4ee03
NuuttiNiemela <nuutti.niemela@hotmail.com>
2019-07-30 14:00:41 +0300

    CRUD-toiminnot valmiit

--- src/main/java/fi/academy/frisbeebackend/repositories/FrisbeeRepository.java
+++ src/main/java/fi/academy/frisbeebackend/repositories/FrisbeeRepository.java
@@ -1,6 +1,7 @@
 package fi.academy.frisbeebackend.repositories;
 
 import fi.academy.frisbeebackend.Frisbee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -11,4 +12,6 @@ import java.util.List;
 public interface FrisbeeRepository extends CrudRepository<Frisbee, Integer> {
     List<Frisbee> findByNameContainsIgnoreCase (@Param("haku") String haku);
     List<Frisbee> findById (@Param("haku") String haku);
+    @Query("SELECT f FROM Frisbee f WHERE f.id = :id")
+    Frisbee findOne (@Param("id") int id);
 }
